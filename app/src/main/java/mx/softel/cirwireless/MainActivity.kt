87d6ba70b00c6a703c6d7a4350c1f5a97cb1924c
Ernesto Vector <egm.droid@gmail.com>
2019-12-20 09:06:40 -0600

    Librería creada y comunicada a la app principal

--- app/src/main/java/mx/softel/cirwireless/MainActivity.kt
+++ app/src/main/java/mx/softel/cirwireless/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import mx.softel.cirwirelesslib.BleUtils
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,25 +16,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        BleUtils.helloWorldLib()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
---
+++ cirwirelesslib/src/main/java/mx/softel/cirwirelesslib/BleUtils.kt
@@ -0,0 +1,13 @@
+package mx.softel.cirwirelesslib
+
+import android.util.Log
+
+object BleUtils {
+
+    private val TAG = BleUtils::class.java.simpleName
+
+    fun helloWorldLib() {
+        Log.d(TAG, "Hola Mundo desde la librería")
+    }
+
+}
