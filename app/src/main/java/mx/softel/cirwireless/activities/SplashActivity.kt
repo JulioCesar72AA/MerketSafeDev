df122564910baab1373123aaf73dbc79410490de
Ernesto Gálvez Martínez <egalvez@softel.mx>
2019-12-26 11:42:04 -0600

    Splash screen

---
+++ app/src/main/java/mx/softel/cirwireless/activities/SplashActivity.kt
@@ -0,0 +1,19 @@
+package mx.softel.cirwireless.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import mx.softel.cirwireless.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
