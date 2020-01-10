87556e11c6dd8c80f369fa250d87e8cb98c04195
Ernesto Gálvez Martínez <egalvez@softel.mx>
2020-01-10 08:33:41 -0600

    Click a ListView y muestra de diálogo de password

--- app/src/main/java/mx/softel/cirwireless/dialogs/PasswordDialog.kt
+++ app/src/main/java/mx/softel/cirwireless/dialogs/PasswordDialog.kt
@@ -1,4 +1,81 @@
 package mx.softel.cirwireless.dialogs
 
-class PasswordDialog {
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+import mx.softel.cirwireless.R
+import mx.softel.cirwireless.extensions.toast
+
+class PasswordDialog: DialogFragment(), View.OnClickListener {
+
+    private lateinit var btnContinue    : Button
+    private lateinit var btnCancel      : Button
+
+    /************************************************************************************************/
+    /**     CICLO DE VIDA                                                                           */
+    /************************************************************************************************/
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Elegimos un tema por defecto de DialogFragment
+        //setStyle(STYLE_NO_TITLE, R.style.CustomDialog)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.pop_up_password, container, false)
+        view.apply {
+            btnContinue = findViewById(R.id.btnAccept)
+            btnCancel   = findViewById(R.id.btnCancel)
+            setOnClickListeners()
+        }
+        return view
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = Dialog(context!!)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return dialog
+    }
+
+
+    private fun setOnClickListeners() {
+        btnCancel.setOnClickListener(this)
+        btnContinue.setOnClickListener(this)
+    }
+
+    /************************************************************************************************/
+    /**     ON CLICK                                                                                */
+    /************************************************************************************************/
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.btnAccept -> setAP()
+            R.id.btnCancel -> cancel()
+        }
+    }
+
+
+    private fun setAP() {
+        toast("Configurando el dispositivo CIR WIRELESS")
+    }
+
+    private fun cancel() {
+        toast("Cancelado")
+    }
+
+
+    /************************************************************************************************/
+    /**     COMPANION OBJECT                                                                        */
+    /************************************************************************************************/
+    companion object {
+        private val TAG = PasswordDialog::class.java.simpleName
+    }
+
 }
--- app/src/main/java/mx/softel/cirwireless/fragments/AccessPointsFragment.kt
+++ app/src/main/java/mx/softel/cirwireless/fragments/AccessPointsFragment.kt
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
 import android.os.Bundle
@@ -11,21 +13,24 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import mx.softel.cirwireless.R
 import mx.softel.cirwireless.activities.RootActivity
+import mx.softel.cirwireless.dialogs.PasswordDialog
+import mx.softel.cirwireless.extensions.toast
 
-class AccessPointsFragment: Fragment() {
+class AccessPointsFragment: Fragment(), AdapterView.OnItemClickListener {
 
     private lateinit var root           : RootActivity
     private lateinit var wifiManager    : WifiManager
 
     private lateinit var wifiResults    : MutableList<ScanResult>
-    private          var arrayList      = ArrayList<String>()
+    private          var apList         = ArrayList<String>()
 
-    private lateinit var adapter        : ArrayAdapter<String>
+    private lateinit var arrayAdapter   : ArrayAdapter<String>
 
     private lateinit var lvAccessPoints : ListView
 
@@ -38,7 +43,6 @@ class AccessPointsFragment: Fragment() {
         root        = (activity!! as RootActivity)
         wifiManager = root.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         wifiManager.isWifiEnabled = true
-
 
         //root.startBleService()
         Log.d(TAG, "Dispositivo a conectar: ${root.bleDevice.address}")
@@ -54,8 +58,11 @@ class AccessPointsFragment: Fragment() {
             lvAccessPoints = findViewById(R.id.lvAccessPoints)
         }
 
-        adapter = ArrayAdapter(root, android.R.layout.simple_list_item_1, arrayList)
-        lvAccessPoints.adapter = adapter
+        arrayAdapter = ArrayAdapter(root, android.R.layout.simple_list_item_1, apList)
+        lvAccessPoints.apply {
+            adapter             = arrayAdapter
+            onItemClickListener = this@AccessPointsFragment
+        }
         scanWifi()
 
         return view
@@ -79,20 +86,33 @@ class AccessPointsFragment: Fragment() {
             for (data in wifiResults) {
                 Log.d(TAG, "${data.SSID} - ${data.frequency} - ${data.centerFreq0} - ${data.centerFreq1}")
                 if (data.frequency < 3000 && data.SSID.length > 2)
-                    if (!arrayList.contains(data.SSID)) arrayList.add(data.SSID)
-                adapter.notifyDataSetChanged()
+                    if (!apList.contains(data.SSID)) apList.add(data.SSID)
+                arrayAdapter.notifyDataSetChanged()
             }
         }
 
     }
 
 
+    /************************************************************************************************/
+    /**     ON CLICK                                                                                */
+    /************************************************************************************************/
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val dialog = PasswordDialog()
+        dialog.apply {
+            show(root.supportFragmentManager, null)
+        }
+
+
+        toast(apList[position])
+    }
+
 
     /************************************************************************************************/
     /**     WIFI                                                                                    */
     /************************************************************************************************/
     private fun scanWifi() {
-        arrayList.clear()
+        apList.clear()
         root.registerReceiver(wifiReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
         wifiManager.startScan()
     }
