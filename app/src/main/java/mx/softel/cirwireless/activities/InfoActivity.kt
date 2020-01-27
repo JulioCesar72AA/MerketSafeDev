e4a01d61b07f52509d76253ed88628058838e32e
Ernesto Gálvez Martínez <egalvez@softel.mx>
2020-01-27 13:58:09 -0600

    Corrección de pantalla de Información de la App

--- app/src/main/java/mx/softel/cirwireless/activities/InfoActivity.kt
+++ app/src/main/java/mx/softel/cirwireless/activities/InfoActivity.kt
@@ -2,16 +2,25 @@ package mx.softel.cirwireless.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_info.*
 import mx.softel.cirwireless.R
 
-class InfoActivity : AppCompatActivity() {
+class InfoActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
         val info = this.packageManager.getPackageInfo(packageName, 0)
         tvVersion.text = resources.getString(R.string.tv_version, info.versionName)
+        ivBackInfo.setOnClickListener(this)
+    }
+
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.ivBackInfo -> { finish() }
+        }
     }
 
 }
